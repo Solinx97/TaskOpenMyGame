13e369fd75179db78d5ae570a420bc0f385569ca
ALEH FIADOSAU <33495591+Solinx97@users.noreply.github.com>
2020-09-22 21:12:41 +0300

    Added Elements generation
    Added generation of levels. When generating a level, the dimension of the grid, the number of elements and their location are indicated.
    The exchange between the two elements is also added.

---
+++ Test Open My Game/Assets/Scripts/ElementsGeneration.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementsGeneration : MonoBehaviour
+{
+    #region UI
+
+    [Header("Grid")]
+
+    [SerializeField]
+    private int _lines = 5;
+    [SerializeField]
+    private int _cells = 5;
+    [SerializeField]
+    private float _stepToX = 55;
+    [SerializeField]
+    private float _stepToY = 55;
+    [Tooltip("Start position for elements generation on the grid")]
+    [SerializeField]
+    private Vector2 _startPosition = new Vector2();
+
+    [Header("Elements")]
+
+    [SerializeField]
+    private GameObject _elementPref = null;
+
+    [Header("Generation")]
+
+    [Tooltip("The cell numbers in which the element is created. Format: 'Line.Cell' (example, '2.3')")]
+    [SerializeField]
+    private string []_cellsNumbers = new string[0];
+
+    #endregion
+
+    private void Start()
+    {
+        var lines = Split(_cellsNumbers, GridType.Line);
+        var cells = Split(_cellsNumbers, GridType.Cell);
+
+        Generation(lines, cells);
+    }
+
+    private void Generation(List<int> linesNumbers, List<int> cellsNumbers)
+    {
+        float toX = 0;
+        float toY = 0;
+
+        for (int i = 0; i < _lines; i++)
+        {
+            for (int j = 0; j < _cells; j++)
+            {
+                if (linesNumbers.Contains(i + 1))
+                {
+                    var currentIndex = linesNumbers.IndexOf(i + 1);
+                    if (j + 1 == cellsNumbers[currentIndex])
+                    {
+                        var position = new Vector2(_startPosition.x + toX, _startPosition.y + toY);
+                        Instantiate(_elementPref, position, _elementPref.transform.rotation, transform);
+
+                        linesNumbers.Remove(linesNumbers[currentIndex]);
+                        cellsNumbers.Remove(cellsNumbers[currentIndex]);
+                    }
+                }
+
+                toX += _stepToX;
+            }
+
+            toY += _stepToY;
+            toX = 0;
+        }
+    }
+
+    private List<int> Split(string[] elementsForSplit, GridType gridType)
+    {
+        var numbers = new List<int>();
+
+        for (int i = 0; i < elementsForSplit.Length; i++)
+        {
+            var split = elementsForSplit[i].Split('.');
+
+            switch (gridType)
+            {
+                case GridType.Line:
+                    numbers.Add(int.Parse(split[0]));
+                    break;
+                case GridType.Cell:
+                    numbers.Add(int.Parse(split[1]));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return numbers;
+    }
+}
---
+++ Test Open My Game/Assets/Scripts/MovementElements.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementElements : MonoBehaviour
+{
+    #region UI
+
+    [Tooltip("Speed swapping two elements")]
+    [SerializeField]
+    private float _speed = 1;
+
+    #endregion
+
+    private List<GameObject> _elementsForSwap = new List<GameObject>();
+    private List<Vector2> _elementsPositions = new List<Vector2>();
+
+    private void Update()
+    {
+        SelectionElements();
+
+        if (_elementsPositions.Count == 2)
+        {
+            var firstDirection = ChoiceDirection(_elementsPositions[0], _elementsPositions[1]);
+            var secondDirection = ChoiceDirection(_elementsPositions[1], _elementsPositions[0]);
+
+            Swapping(firstDirection, secondDirection);
+        }
+    }
+
+    private void SelectionElements()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var info = Physics2D.OverlapPoint(ray);
+            if (info != null)
+            {
+                _elementsForSwap.Add(info.gameObject);
+                _elementsPositions.Add(info.gameObject.transform.position);
+            }
+        }
+    }
+
+    private void Swapping(DirectionType first, DirectionType second)
+    {
+        bool firstSwaped = false;
+        bool secondSwapped = false;
+
+        if (_elementsForSwap.Count == 2)
+        {
+            firstSwaped = Moving(_elementsForSwap[0], _elementsPositions[1], first);
+            secondSwapped = Moving(_elementsForSwap[1], _elementsPositions[0], second);
+        }
+
+        if (firstSwaped && secondSwapped)
+        {
+            _elementsForSwap.Clear();
+            _elementsPositions.Clear();
+        }
+    }
+
+    private bool Moving(GameObject obj, Vector3 targetPosition, DirectionType direction)
+    {
+        var position = obj.transform.position;
+        bool isSwaped = false;
+
+        if (position != targetPosition)
+        {
+            switch (direction)
+            {
+                case DirectionType.Right:
+                    obj.transform.position = new Vector3(position.x + _speed, position.y, position.z);
+                    break;
+                case DirectionType.Left:
+                    obj.transform.position = new Vector3(position.x - _speed, position.y, position.z);
+                    break;
+                case DirectionType.Top:
+                    obj.transform.position = new Vector3(position.x, position.y + _speed, position.z);
+                    break;
+                case DirectionType.Bottom:
+                    obj.transform.position = new Vector3(position.x, position.y - _speed, position.z);
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            isSwaped = true;
+        }
+
+        return isSwaped;
+    }
+
+    private DirectionType ChoiceDirection(Vector2 firstPosition, Vector2 secondPosition)
+    {
+        float differenceToX = firstPosition.x - secondPosition.x;
+
+        DirectionType direction;
+        if (differenceToX > 0)
+        {
+            direction = DirectionType.Left;
+        }
+        else if (differenceToX < 0)
+        {
+            direction = DirectionType.Right;
+        }
+        else
+        {
+            float differenceToY = firstPosition.y - secondPosition.y;
+            if (differenceToY > 0)
+            {
+                direction = DirectionType.Bottom;
+            }
+            else
+            {
+                direction = DirectionType.Top;
+            }
+        }
+
+        return direction;
+    }
+}
